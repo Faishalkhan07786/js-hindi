522affed1c94add86c50de40c15ca5555ed7d74c
Faishalkhan07786 <mdfaishalkhan0786@gmail.com>
2025-04-26 19:35:56 +0000

    varibales in js done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 1234;
+let accountEmail = "faishal@google.in";
+var accountPassword = "34256";
+accountCity = "meerut";
+let accountState;
+
+
+// accountId = 2345; not allowed
+
+accountEmail = "afzal@gmail.com";
+accountPassword = "1234";
+accountCity ="delhi";
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
