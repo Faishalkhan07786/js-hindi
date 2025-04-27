4b02eba4b99d4889a05d4b0a61169993a31e0854
Faishalkhan07786 <mdfaishalkhan0786@gmail.com>
2025-04-27 18:33:03 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"; /*treat all JS code as newer version*/
+
+// alert(4 + 3)  we are using nodejs, not browser
+
+console.log(4 
+    + 
+    3)  /*code readability should be high*/
+
+console.log("faishal")
+
+let name = "faishal"
+let age = 25
+let isLoggedIn = true
+
+
+// DATATYPE
+
+// 1 number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assign
+// symbol => unique
+
+// object
+
+console.log(typeof null); /* object*/
+
+console.log(typeof undefined); /* undefined */
+
+
