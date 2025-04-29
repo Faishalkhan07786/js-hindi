4440f179451289abf839513f4e37a4d06323b774
Faishalkhan07786 <mdfaishalkhan0786@gmail.com>
2025-04-29 18:53:25 +0000

    datatype summary

---
+++ 01_basics/05_datatypeSummary.js
@@ -0,0 +1,34 @@
+// Datatypes
+
+// Primitive
+
+// 7types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100;
+const scorevalue = 100.3;
+
+const isLoggedIn = true;
+const insideTemp = null;
+let userEmail;
+
+console.log(typeof userEmail);
+
+const id = Symbol("123")
+const anotherId = Symbol("123");
+const bigNumber = 34253658608473626n
+
+// Reference (Non Primitive)
+
+// Array, Object, Functions
+
+const superHeros = ["superman", "spiderman", "ironman"];
+let student = {
+    name: "faishal khan",
+    rollNo: 100,
+};
+
+const myFunction = function() {
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber);
