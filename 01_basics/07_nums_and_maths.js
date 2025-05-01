6a92b234fe13ddec885bd2ed4af057d9b2a3f30d
Faishalkhan07786 <mdfaishalkhan0786@gmail.com>
2025-05-01 18:54:38 +0000

    numbers ans maths operation

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.567;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// ++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(6.7));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.2));
+// console.log(Math.min(5,2,9,3,7,11,6,1,18));
+// console.log(Math.max(5,2,9,3,7,11,6,1,18));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+
+
+
+
+
+
