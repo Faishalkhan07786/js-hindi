69b45870cb78921ce0e5ad090708525a70867aa9
Faishalkhan07786 <mdfaishalkhan0786@gmail.com>
2025-05-02 19:52:59 +0000

    array in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,16 @@
+// Arrays
+
+const arr = [0, 1, 2 ,3 ,4, 5];
+const myHeros = ["spiderman", "superman", "ironman"]
+
+const arr2 = new Array(1, 2, 3, 4);
+
+console.log(arr[3]);
+
+// Array Methods
+
+arr.push(6);
+arr.push(8);
+
+console.log(arr);
+
