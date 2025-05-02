e33ad6a8a88e8a2e04065beade4fe530ae8b2375
Faishalkhan07786 <mdfaishalkhan0786@gmail.com>
2025-05-02 18:50:32 +0000

    dates in js

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,37 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2025,0,3);
+// let myCreatedDate = new Date(2025,0,3,4,5);
+// let myCreatedDate = new Date("2025-05-03");
+let myCreatedDate = new Date("03-05-2025");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+
+console.log(newDate.getDate());
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
+
+
+
+
+
+
